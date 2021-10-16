25edebf59bf5f685a265aee520fcf1cffa040c14
DRASURHDR <80607993+DRASURHDR@users.noreply.github.com>
2021-10-16 13:55:29 +0700

    h

---
+++ js/products.js
@@ -0,0 +1,89 @@
+let products = [
+    {
+        name: 'JBL E55BT KEY BLACK',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'JBL JR 310BT',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'JBL TUNE 750BTNC',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'JBL Horizon',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'JBL Tune 220TWS',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'UA Project Rock',
+        image1: '',
+        image2: '',
+        old_price: '400',
+        curr_price: '300'
+    },
+]
+
+let product_list = document.querySelector('#products')
+
+renderProducts = (products) => {
+    products.forEach(e => {
+        let prod = `
+            <div class="col-4 col-md-6 col-sm-12">
+                <div class="product-card">
+                    <div class="product-card-img">
+                        <img src="${e.image1}" alt="">
+                        <img src="${e.image2}" alt="">
+                    </div>
+                    <div class="product-card-info">
+                        <div class="product-btn">
+                            <a href="./product-d.html" class="btn-flat btn-hover btn-shop-now">shop now</a>
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-cart-add'></i>
+                            </button>
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-heart'></i>
+                            </button>
+                        </div>
+                        <div class="product-card-name">
+                            ${e.name}
+                        </div>
+                        <div class="product-card-price">
+                            <span><del>${e.old_price}</del></span>
+                            <span class="curr-price">${e.curr_price}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        product_list.insertAdjacentHTML("beforeend", prod)
+    })
+}
+
+renderProducts(products)
+renderProducts(products)
+
+let filter_col = document.querySelector('#filter-col')
+
+document.querySelector('#filter-toggle').addEventListener('click', () => filter_col.classList.toggle('active'))
+
+document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'))
